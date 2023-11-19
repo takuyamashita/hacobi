afa95b50800674566845331fcb0f99efb54ab668
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-19 21:54:44 +0900

    ネストしたトランザクションに対応するmysqlを作成

---
+++ src/api/pkg/db/mysql.go
@@ -0,0 +1,96 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+type MySQL struct {
+	*sql.DB
+	tx      *sql.Tx
+	txCount uint
+}
+
+func NewMySQL(db *sql.DB) MySQL {
+	return MySQL{
+		DB: db,
+	}
+}
+
+type txFunc func(tx *MySQL) error
+
+func (m *MySQL) BeginTx(ctx context.Context, opts *sql.TxOptions) (*MySQL, error) {
+
+	m.txCount++
+
+	if m.tx != nil {
+		return m, nil
+	}
+
+	tx, err := m.DB.BeginTx(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	m.tx = tx
+
+	return m, nil
+}
+
+func (m *MySQL) Commit() error {
+
+	m.txCount--
+
+	if m.txCount > 0 {
+		return nil
+	}
+
+	err := m.tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	m.tx = nil
+
+	return nil
+}
+
+func (m *MySQL) Rollback() error {
+
+	err := m.tx.Rollback()
+	if err != nil {
+		return err
+	}
+
+	m.txCount = 0
+	m.tx = nil
+
+	return nil
+}
+
+func (m *MySQL) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+
+	if m.tx != nil {
+		return m.tx.ExecContext(ctx, query, args...)
+	}
+
+	return m.DB.ExecContext(ctx, query, args...)
+}
+
+func (m *MySQL) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+
+	if m.tx != nil {
+		return m.tx.QueryContext(ctx, query, args...)
+	}
+
+	return m.DB.QueryContext(ctx, query, args...)
+}
+
+func (m *MySQL) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
+
+	if m.tx != nil {
+		return m.tx.QueryRowContext(ctx, query, args...)
+	}
+
+	return m.DB.QueryRowContext(ctx, query, args...)
+}
