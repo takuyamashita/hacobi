bba7afe6f3cdd93fe8ee3745c77d6be7bdbe9ce9
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-19 22:14:53 +0900

    refactor

--- src/api/cmd/web/dependence.go
+++ src/api/cmd/web/dependence.go
@@ -12,6 +12,10 @@ func (app *application) setupDI() {
 
 	//~~~~~~~~~~~~~~~~~~ repository ~~~~~~~~~~~~~~~~~~//
 
+	app.container.Bind(func() usecase.UuidRepositoryIntf {
+		return repository.NewUuidRepository()
+	})
+
 	app.container.Bind(func() live_house_staff_domain.LiveHouseStaffRepositoryIntf {
 		return repository.NewliveHouseStaff(app.db)
 	})
@@ -20,16 +24,9 @@ func (app *application) setupDI() {
 		return repository.NewliveHouseStaff(app.db)
 	})
 
-	app.container.Bind(func() usecase.UuidRepositoryIntf {
-		return repository.NewUuidRepository()
-	})
-
 	app.container.Bind(func() usecase.LiveHouseAccountRepositoryIntf {
 		return repository.NewliveHouseAccount(app.db)
 	})
-
-	// or
-	// app.container.Bind(repository.NewUuidRepository)
 
 	//~~~~~~~~~~~~~~~~~~ domain ~~~~~~~~~~~~~~~~~~//
 
@@ -39,17 +36,6 @@ func (app *application) setupDI() {
 
 	//~~~~~~~~~~~~~~~~~~ usecase ~~~~~~~~~~~~~~~~~~//
 
-	app.container.Bind(func(
-		uuidRepository usecase.UuidRepositoryIntf,
-		liveHouseStaffRepository usecase.LiveHouseStaffRepositoryIntf,
-		liveHouseStaffEmailAddressChecker live_house_staff_domain.LiveHouseStaffEmailAddressCheckerIntf,
-	) live_house_staff_usecase.LiveHouseStaffUsecaseIntf {
-		return live_house_staff_usecase.NewLiveHouseStaffUsecase(
-			uuidRepository,
-			liveHouseStaffRepository,
-			liveHouseStaffEmailAddressChecker,
-		)
-	})
-
+	app.container.Bind(live_house_staff_usecase.NewLiveHouseStaffUsecase)
 	app.container.Bind(live_house_account_usecase.NewLiveHouseAccountUsecase)
 }
