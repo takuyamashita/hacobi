4eaa0819b6df1afb58653d7b7e6ea67ed438a0f3
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-24 02:27:46 +0900

    add comment

--- src/api/pkg/webauthn/PublicKeyCredentialCreationOptions.go
+++ src/api/pkg/webauthn/PublicKeyCredentialCreationOptions.go
@@ -1,5 +1,8 @@
 package webauthn
 
+// registration
+// https://www.w3.org/TR/webauthn/#sctn-registering-a-new-credential
+
 // https://www.w3.org/TR/webauthn/#dictdef-publickeycredentialcreationoptions
 type PublicKeyCredentialCreationOptions struct {
 	rp   PublicKeyCreadentialRpEntity
