e427a6ed06fd2d48f11b1753567f55cc1504d2e4
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-23 21:33:51 +0900

    fileを分割

--- src/api/pkg/usecase/register_live_house_staff.go
+++ src/api/pkg/usecase/register_live_house_staff.go
@@ -5,38 +5,8 @@ import (
 	"errors"
 
 	"github.com/takuyamashita/hacobi/src/api/pkg/container"
-	"github.com/takuyamashita/hacobi/src/api/pkg/domain/event"
 	"github.com/takuyamashita/hacobi/src/api/pkg/domain/live_house_staff_domain"
-	"github.com/takuyamashita/hacobi/src/api/pkg/domain/live_house_staff_email_authorization_domain"
 )
-
-func SendLiveHouseStaffEmailAuthorization(emailAddress string, ctx context.Context, container container.Container) error {
-
-	var (
-		liveHouseStaffEmailAuthorizationRepo LiveHouseStaffEmailAuthorizationRepositoryIntf
-		tokenGenerator                       live_house_staff_email_authorization_domain.TokenGeneratorIntf
-		eventPublisher                       event.EventPublisherIntf[live_house_staff_email_authorization_domain.AuthCreatedEvent]
-	)
-	container.Make(&liveHouseStaffEmailAuthorizationRepo)
-	container.Make(&tokenGenerator)
-	container.Make(&eventPublisher)
-
-	token, err := tokenGenerator.Generate()
-	if err != nil {
-		return err
-	}
-
-	auth, err := live_house_staff_email_authorization_domain.NewLiveHouseStaffEmailAuthorization(emailAddress, token.String())
-	if err != nil {
-		return err
-	}
-
-	liveHouseStaffEmailAuthorizationRepo.Save(auth, ctx)
-
-	eventPublisher.Publish(live_house_staff_email_authorization_domain.AuthCreatedEvent{})
-
-	return nil
-}
 
 func RegisterLiveHouseStaff(name string, emailAddress string, password string, ctx context.Context, container container.Container) (string, error) {
 
---
+++ src/api/pkg/usecase/send_live_house_staff_email_authorization.go
@@ -0,0 +1,42 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/takuyamashita/hacobi/src/api/pkg/container"
+	"github.com/takuyamashita/hacobi/src/api/pkg/domain/event"
+	"github.com/takuyamashita/hacobi/src/api/pkg/domain/live_house_staff_email_authorization_domain"
+)
+
+func SendLiveHouseStaffEmailAuthorization(emailAddress string, ctx context.Context, container container.Container) error {
+
+	var (
+		liveHouseStaffEmailAuthorizationRepo LiveHouseStaffEmailAuthorizationRepositoryIntf
+		tokenGenerator                       live_house_staff_email_authorization_domain.TokenGeneratorIntf
+		eventPublisher                       event.EventPublisherIntf[live_house_staff_email_authorization_domain.AuthCreatedEvent]
+	)
+	container.Make(&liveHouseStaffEmailAuthorizationRepo)
+	container.Make(&tokenGenerator)
+	container.Make(&eventPublisher)
+
+	token, err := tokenGenerator.Generate()
+	if err != nil {
+		return err
+	}
+
+	auth, err := live_house_staff_email_authorization_domain.NewLiveHouseStaffEmailAuthorization(emailAddress, token.String())
+	if err != nil {
+		return err
+	}
+
+	liveHouseStaffEmailAuthorizationRepo.Save(auth, ctx)
+
+	event := live_house_staff_email_authorization_domain.AuthCreatedEvent{
+		To:           auth.Token(),
+		EmailAddress: auth.EmailAddress(),
+	}
+
+	eventPublisher.Publish(event)
+
+	return nil
+}
