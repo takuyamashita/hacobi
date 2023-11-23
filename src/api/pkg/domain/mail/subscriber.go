8e9f00e3f96597c6491479461f5bfa3e8fca45f4
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-23 23:03:45 +0900

    メール本文に登録URLを記載

--- src/api/pkg/domain/mail/subscriber.go
+++ src/api/pkg/domain/mail/subscriber.go
@@ -1,6 +1,9 @@
 package mail
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/takuyamashita/hacobi/src/api/pkg/domain"
 	"github.com/takuyamashita/hacobi/src/api/pkg/domain/live_house_staff_email_authorization_domain"
 )
@@ -17,5 +20,9 @@ func NewLiveHouseStaffEmailAuthorization(m domain.MailIntf) LiveHouseStaffEmailA
 
 func (e LiveHouseStaffEmailAuthorization) Handle(event live_house_staff_email_authorization_domain.AuthCreatedEvent) {
 
-	e.mail.Send(event.EmailAddress.String(), "認証メール", "認証メールの本文")
+	e.mail.Send(
+		event.EmailAddress.String(),
+		"認証メール",
+		fmt.Sprintf("%s/live_house_staff_register/?token=%s", os.Getenv("DOMAIN"), event.Token.String()),
+	)
 }
