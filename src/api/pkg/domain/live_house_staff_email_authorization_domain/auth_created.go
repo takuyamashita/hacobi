a9a1ab6e5a6d64a885a908bbccc16e23d96b0863
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-23 21:32:43 +0900

    fix typo

--- src/api/pkg/domain/live_house_staff_email_authorization_domain/auth_created.go
+++ src/api/pkg/domain/live_house_staff_email_authorization_domain/auth_created.go
@@ -3,6 +3,6 @@ package live_house_staff_email_authorization_domain
 import "github.com/takuyamashita/hacobi/src/api/pkg/domain"
 
 type AuthCreatedEvent struct {
-	To           Token
+	Token        Token
 	EmailAddress domain.LiveHouseStaffEmailAddress
 }
