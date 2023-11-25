aea40888d926d09b6a61689f944a8075c51ed596
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-25 17:02:49 +0900

    add PublicKeyCredentialCreationOptions

--- src/api/pkg/webauthn/public_key_credential_creation_options.go
+++ src/api/pkg/webauthn/public_key_credential_creation_options.go
@@ -7,4 +7,13 @@ package webauthn
 type PublicKeyCredentialCreationOptions struct {
 	rp   PublicKeyCreadentialRpEntity
 	user PublicKeyCredentialUserEntity
+
+	challenge        Challenge
+	pubKeyCredParams []PublicKeyCredentialParameters
+
+	timeout uint
+
+	// このメンバは、単一の認証子で同じアカウントに対する複数のクレデンシャルの作成を制限したい依拠当事者による使用を意図している。
+	// 新しいクレデンシャルが、このパラメータに列挙されたクレデンシャルのいずれかを含む認証機で作成される場合、クライアントはエラーを返すように要求される。
+	excludeCredentials []PublicKeyCredentialDescriptor
 }
