eb070b3a30b550193066148a5621edaeaf99d751
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-28 22:09:36 +0900

    fix typo

--- src/api/migration/20231112311345_create_live_house_staff_account_provisional_registrations.up.sql
+++ src/api/migration/20231112311345_create_live_house_staff_account_provisional_registrations.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS live_house_staff_account_provisional_registration (
+CREATE TABLE IF NOT EXISTS live_house_staff_account_provisional_registrations (
     live_house_staff_account_id VARCHAR(36) NOT NULL,
     token VARCHAR(128) NOT NULL,
     created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
