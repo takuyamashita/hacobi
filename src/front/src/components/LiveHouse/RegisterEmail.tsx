b88f57c07d92665cf4593eeb0efa82720f82fb41
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-11-30 03:01:57 +0900

    error handling

--- src/front/src/components/LiveHouse/RegisterEmail.tsx
+++ src/front/src/components/LiveHouse/RegisterEmail.tsx
@@ -5,21 +5,36 @@ import Button from "../Button";
 
 const RegisterEmail = () => {
   const [emailAddress, setEmailAddress] = React.useState<string>("");
+  const [errorMessages, setErrorMessages] = React.useState<string[]>([]);
 
   const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement> | React.MouseEvent,
   ) => {
     e.preventDefault();
 
-    const res = await fetch("/api/v1/send_live_house_staff_email_authorization", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        emailAddress,
-      }),
-    });
+    try {
+      const res = await fetch(
+        "/api/v1/send_live_house_staff_email_authorization",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            emailAddress,
+          }),
+        },
+      );
+
+      if (!res.ok) {
+        throw res;
+      }
+    } catch (err) {
+      if (err instanceof Response) {
+        const json = await err.json();
+        setErrorMessages((prev) => [...prev, json.message]);
+      }
+    }
   };
 
   return (
@@ -27,6 +42,14 @@ const RegisterEmail = () => {
       <p>RegisterEmail</p>
       <form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="email">Email</label>
+        {errorMessages.map((message) => (
+          <p
+            className="text-orange-500"
+            key={message}
+          >
+            {message}
+          </p>
+        ))}
         <input
           type="email"
           id="email"
