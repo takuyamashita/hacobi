f6f313d318b177ef1d7843e443e75e567f325cba
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-12-03 18:12:40 +0900

    refactor

--- src/api/pkg/repository/live_house_staff_account.go
+++ src/api/pkg/repository/live_house_staff_account.go
@@ -27,6 +27,160 @@ func NewLiveHouseStaffAccountRepository(db *db.MySQL) LiveHouseStaffAccountRepos
 	}
 }
 
+func int2Bool(i int) bool {
+	if i == 1 {
+		return true
+	}
+	return false
+}
+
+type liveHouseStaffAccountSelectStmtTmpl string
+type liveHouseStaffAccountSelectStmtTmplArgs func() []interface{}
+
+const liveHouseStaffAccountSelectStmt liveHouseStaffAccountSelectStmtTmpl = `
+		SELECT
+			a.id,
+			a.email,
+			a.is_provisional,
+			r.token,
+			r.created_at,
+			c.challenge,
+			c.created_at
+		FROM 
+			live_house_staff_accounts
+			AS
+				a
+		LEFT JOIN 
+			live_house_staff_account_provisional_registrations 
+			AS
+				r
+			ON
+				a.id = r.live_house_staff_account_id
+		LEFT JOIN
+			live_house_staff_account_credential_challenges
+			AS
+				c
+			ON
+				a.id = c.live_house_staff_account_id	
+		WHERE %s.%s = ?
+`
+
+func (stmt liveHouseStaffAccountSelectStmtTmpl) bind(
+	tableName string,
+	columnName string,
+	value string,
+) (liveHouseStaffAccountSelectStmtTmpl, liveHouseStaffAccountSelectStmtTmplArgs) {
+
+	s := fmt.Sprintf(string(stmt), tableName, columnName)
+
+	return liveHouseStaffAccountSelectStmtTmpl(s), func() []interface{} {
+		return []interface{}{value}
+	}
+}
+
+func (repo LiveHouseStaffAccountRepositoryImpl) findBy(
+	stmt liveHouseStaffAccountSelectStmtTmpl,
+	args liveHouseStaffAccountSelectStmtTmplArgs,
+	ctx context.Context,
+) (account live_house_staff_account_domain.LiveHouseStaffAccountIntf, err error) {
+
+	var (
+		accountId          string
+		email              string
+		isProvisional      int
+		token              sql.NullString
+		tokenCreatedAt     sql.NullTime
+		challenge          sql.NullString
+		challengeCreatedAt sql.NullTime
+	)
+
+	row := repo.db.QueryRowContext(
+		ctx,
+		string(stmt),
+		args()...,
+	)
+
+	if err := row.Scan(
+		&accountId,
+		&email,
+		&isProvisional,
+		&token,
+		&tokenCreatedAt,
+		&challenge,
+		&challengeCreatedAt,
+	); err != nil {
+
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	var (
+		provisionalRegistrationParam *live_house_staff_account_domain.ProvisionalRegistrationParam
+		credentialChallengeParam     *live_house_staff_account_domain.CredentialChallengeParam
+	)
+
+	if token.Valid {
+
+		provisionalRegistrationParam = &live_house_staff_account_domain.ProvisionalRegistrationParam{
+			Token:     token.String,
+			CreatedAt: tokenCreatedAt.Time,
+		}
+	}
+
+	if challenge.Valid {
+
+		credentialChallengeParam = &live_house_staff_account_domain.CredentialChallengeParam{
+			Challenge: challenge.String,
+			CreatedAt: challengeCreatedAt.Time,
+		}
+	}
+
+	account, err = live_house_staff_account_domain.NewLiveHouseStaffAccount(live_house_staff_account_domain.NewLiveHouseStaffAccountParams{
+		Id:                        accountId,
+		Email:                     email,
+		IsProvisional:             int2Bool(isProvisional),
+		ProvisionalRegistration:   provisionalRegistrationParam,
+		CredentialChallengeParams: credentialChallengeParam,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return account, nil
+}
+
+func (repo LiveHouseStaffAccountRepositoryImpl) FindByEmail(
+	emailAddress domain.LiveHouseStaffEmailAddress,
+	ctx context.Context,
+) (live_house_staff_account_domain.LiveHouseStaffAccountIntf, error) {
+
+	stmt, args := liveHouseStaffAccountSelectStmt.bind("a", "email", emailAddress.String())
+
+	return repo.findBy(stmt, args, ctx)
+}
+
+func (repo LiveHouseStaffAccountRepositoryImpl) FindById(
+	id live_house_staff_account_domain.LiveHouseStaffAccountId,
+	ctx context.Context,
+) (live_house_staff_account_domain.LiveHouseStaffAccountIntf, error) {
+
+	stmt, args := liveHouseStaffAccountSelectStmt.bind("a", "id", id.String())
+
+	return repo.findBy(stmt, args, ctx)
+}
+
+func (repo LiveHouseStaffAccountRepositoryImpl) FindByProvisionalRegistrationToken(
+	token live_house_staff_account_domain.Token,
+	ctx context.Context,
+) (live_house_staff_account_domain.LiveHouseStaffAccountIntf, error) {
+
+	stmt, args := liveHouseStaffAccountSelectStmt.bind("r", "token", token.String())
+
+	return repo.findBy(stmt, args, ctx)
+}
+
 func (repo LiveHouseStaffAccountRepositoryImpl) Save(
 	account live_house_staff_account_domain.LiveHouseStaffAccountIntf,
 	ctx context.Context,
@@ -119,202 +273,4 @@ func (repo LiveHouseStaffAccountRepositoryImpl) Save(
 
 	return tx.Commit()
 
-}
-
-func (repo LiveHouseStaffAccountRepositoryImpl) FindByEmail(
-	emailAddress domain.LiveHouseStaffEmailAddress,
-	ctx context.Context,
-) (live_house_staff_account_domain.LiveHouseStaffAccountIntf, error) {
-
-	params := newFindByEmailParams(emailAddress)
-
-	return repo.findBy(params, ctx)
-}
-
-func (repo LiveHouseStaffAccountRepositoryImpl) FindById(
-	id live_house_staff_account_domain.LiveHouseStaffAccountId,
-	ctx context.Context,
-) (live_house_staff_account_domain.LiveHouseStaffAccountIntf, error) {
-
-	params := newFindByIdParams(id)
-
-	return repo.findBy(params, ctx)
-}
-
-func (repo LiveHouseStaffAccountRepositoryImpl) FindByProvisionalRegistrationToken(
-	token live_house_staff_account_domain.Token,
-	ctx context.Context,
-) (live_house_staff_account_domain.LiveHouseStaffAccountIntf, error) {
-
-	params := newFindByProvisionalRegistrationTokenParams(token)
-
-	return repo.findBy(params, ctx)
-}
-
-const (
-	findByTypeEmail findByType = "email"
-	findByTypeId    findByType = "id"
-	findByTypeToken findByType = "token"
-
-	selectStmtTmpl = `
-		SELECT
-			a.id,
-			a.email,
-			a.is_provisional,
-			r.token,
-			r.created_at,
-			c.challenge,
-			c.created_at
-		FROM 
-			live_house_staff_accounts
-			AS
-				a
-		LEFT JOIN 
-			live_house_staff_account_provisional_registrations 
-			AS
-				r
-			ON
-				a.id = r.live_house_staff_account_id
-		LEFT JOIN
-			live_house_staff_account_credential_challenges
-			AS
-				c
-			ON
-				a.id = c.live_house_staff_account_id	
-		WHERE %s.%s = %s
-	`
-)
-
-type findByType string
-
-func (t findByType) stmt() string {
-
-	switch t {
-	case findByTypeId:
-		return fmt.Sprintf(selectStmtTmpl, "a", "id", "?")
-	case findByTypeEmail:
-		return fmt.Sprintf(selectStmtTmpl, "a", "email", "?")
-	case findByTypeToken:
-		return fmt.Sprintf(selectStmtTmpl, "r", "token", "?")
-	default:
-		return ""
-	}
-}
-
-type findByParams struct {
-	findByType  findByType
-	id          live_house_staff_account_domain.LiveHouseStaffAccountId
-	email       domain.LiveHouseStaffEmailAddress
-	registToken live_house_staff_account_domain.Token
-}
-
-func newFindByIdParams(id live_house_staff_account_domain.LiveHouseStaffAccountId) findByParams {
-	return findByParams{
-		findByType: findByTypeId,
-		id:         id,
-	}
-}
-
-func newFindByEmailParams(email domain.LiveHouseStaffEmailAddress) findByParams {
-	return findByParams{
-		findByType: findByTypeEmail,
-		email:      email,
-	}
-}
-
-func newFindByProvisionalRegistrationTokenParams(token live_house_staff_account_domain.Token) findByParams {
-	return findByParams{
-		findByType:  findByTypeToken,
-		registToken: token,
-	}
-}
-
-func (p findByParams) args() []interface{} {
-
-	switch p.findByType {
-	case findByTypeId:
-		return []interface{}{p.id.String()}
-	case findByTypeEmail:
-		return []interface{}{p.email.String()}
-	case findByTypeToken:
-		return []interface{}{p.registToken.String()}
-	default:
-		return []interface{}{}
-	}
-}
-
-func int2Bool(i int) bool {
-	if i == 1 {
-		return true
-	}
-	return false
-}
-
-func (repo LiveHouseStaffAccountRepositoryImpl) findBy(params findByParams, ctx context.Context) (account live_house_staff_account_domain.LiveHouseStaffAccountIntf, err error) {
-
-	var (
-		accountId          string
-		email              string
-		isProvisional      int
-		token              sql.NullString
-		tokenCreatedAt     sql.NullTime
-		challenge          sql.NullString
-		challengeCreatedAt sql.NullTime
-	)
-
-	row := repo.db.QueryRowContext(
-		ctx,
-		params.findByType.stmt(),
-		params.args()...,
-	)
-
-	if err := row.Scan(
-		&accountId,
-		&email,
-		&isProvisional,
-		&token,
-		&tokenCreatedAt,
-		&challenge,
-		&challengeCreatedAt,
-	); err != nil {
-
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, nil
-		}
-
-		return nil, err
-	}
-
-	var (
-		provisionalRegistrationParam *live_house_staff_account_domain.ProvisionalRegistrationParam
-		credentialChallengeParam     *live_house_staff_account_domain.CredentialChallengeParam
-	)
-
-	if token.Valid {
-
-		provisionalRegistrationParam = &live_house_staff_account_domain.ProvisionalRegistrationParam{
-			Token:     token.String,
-			CreatedAt: tokenCreatedAt.Time,
-		}
-	}
-
-	if challenge.Valid {
-
-		credentialChallengeParam = &live_house_staff_account_domain.CredentialChallengeParam{
-			Challenge: challenge.String,
-			CreatedAt: challengeCreatedAt.Time,
-		}
-	}
-
-	account, err = live_house_staff_account_domain.NewLiveHouseStaffAccount(live_house_staff_account_domain.NewLiveHouseStaffAccountParams{
-		Id:                        accountId,
-		Email:                     email,
-		IsProvisional:             int2Bool(isProvisional),
-		ProvisionalRegistration:   provisionalRegistrationParam,
-		CredentialChallengeParams: credentialChallengeParam,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return account, nil
 }
