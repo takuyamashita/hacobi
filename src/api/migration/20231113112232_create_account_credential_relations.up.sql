10ed7fa4ca11caac2a69c7bc9591bf467c7575ac
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-12-03 18:34:40 +0900

    add relation table

---
+++ src/api/migration/20231113112232_create_account_credential_relations.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS account_credential_relations (
+    live_house_staff_account_id VARCHAR(36) NOT NULL,
+    public_key_id VARCHAR(128) NOT NULL,
+
+    INDEX (live_house_staff_account_id),
+    FOREIGN KEY (live_house_staff_account_id) REFERENCES live_house_staff_accounts (id) ON DELETE CASCADE,
+
+    INDEX (public_key_id),
+    FOREIGN KEY (public_key_id) REFERENCES account_credentials (public_key_id) ON DELETE CASCADE
+);
